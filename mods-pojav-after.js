e89a68f858164de160e924274de53db72f8b8335
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2024-09-20 00:33:47 -0300

    Create mods-pojav-after.js

---
+++ mods-pojav-after.js
@@ -0,0 +1,13 @@
+1.0|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/1.7.10bspkrsCore-universal-6.12.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/Armourers-Workshop-1.7.10-0.48.5.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/CustomNPC-Plus-1.8.6.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/Dbc Warriors V1.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/DragonBlockC-v1.4.85.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JBRA-Client-v1.6.52.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JRMCore-v1.3.51.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JYearsC-v1.2.3.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar
+|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/Waila-1.5.10_1.7.10.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/WarriorsMOD.jar
