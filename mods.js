7a5897f112f0d62d75a751054a940653ab47895a
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2024-09-19 23:31:57 -0300

    Add files via upload

---
+++ mods.js
@@ -0,0 +1,3 @@
+1.0|
+https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/Armourers.jar|
+https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/CustomNPC-Plus-1.9.2.jar
