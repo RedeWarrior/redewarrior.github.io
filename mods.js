f05dc4bac47700c27a46ecc7bbfda1ff97c09cdc
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2024-09-20 00:14:31 -0300

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,3 +1,5 @@
 1.0|
 https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/Armourers.jar|
-https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/CustomNPC-Plus-1.9.2.jar
+https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/CustomNPC-Plus-1.9.2.jar|
+https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/ninjinentities-Unofficial-1.3.5.2.jar|
+https://raw.githubusercontent.com/Nonopichy/nonopichy.github.io/main/NBArmors-2.7.jar
