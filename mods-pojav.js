bc91ab6c665c2a1f3e61c166450727a6098453a8
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2024-10-14 22:16:05 -0300

    Update mods-pojav.js

--- mods-pojav.js
+++ mods-pojav.js
@@ -1,14 +1,24 @@
-2.0|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/1.7.10bspkrsCore-universal-6.12.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/Armourers-Workshop-1.7.10-0.48.5.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/CustomNPC-Plus-1.8.6.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/Dbc Warriors V1.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/DragonBlockC-v1.4.85.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JBRA-Client-v1.6.52.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JRMCore-v1.3.51.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JYearsC-v1.2.3.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar
-|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/Waila-1.5.10_1.7.10.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/after/WarriorsMOD.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/resourcepacks/dbcwarriors.zip@path=resourcepacks
+1.0 Z|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/+unimixins-all-1.7.10-0.1.17.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/1.7.10bspkrsCore-universal-6.12.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Armourers-Workshop-1.7.10-0.48.5.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/CustomMainMenu-MC1.7.10-1.9.2.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/CustomNPC-Plus-1.9.2.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Dbc Warriors V1.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Dragon-Block.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/entityculling-1.6.4-mc1.7.10.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JBRA.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JFamilyC-v1.2.18.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/jinryuujrmcwarriors.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JYearsC-v1.2.3.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/NBArmors-2.7.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/ninjinentities-Unofficial-1.3.5.2.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/OptiFine-1.7.10_HD_U_E7.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Waila-1.5.10_1.7.10.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/WarriorsMOD.jar|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/resourcepacks/dbcwarriors.zip@path=resourcepacks|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/mainmenu.json@path=config/CustomMainMenu|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/options.txt@path=mods/..|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/optionsof.txt@path=mods/..|
+https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/servers.dat@path=mods/..
