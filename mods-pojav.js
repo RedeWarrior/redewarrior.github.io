2690717b2d52b4412fc17b10efa1cb8cd50c87b3
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2024-10-31 11:26:20 -0300

    Update mods-pojav.js

--- mods-pojav.js
+++ mods-pojav.js
@@ -1,24 +1,24 @@
-1.0 Z|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/+unimixins-all-1.7.10-0.1.17.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/1.7.10bspkrsCore-universal-6.12.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Armourers-Workshop-1.7.10-0.48.5.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/CustomMainMenu-MC1.7.10-1.9.2.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/CustomNPC-Plus-1.9.2.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Dbc Warriors V1.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Dragon-Block.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/entityculling-1.6.4-mc1.7.10.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JBRA.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JFamilyC-v1.2.18.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/jinryuujrmcwarriors.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/JYearsC-v1.2.3.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/NBArmors-2.7.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/ninjinentities-Unofficial-1.3.5.2.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/OptiFine-1.7.10_HD_U_E7.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/Waila-1.5.10_1.7.10.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/mods/pojav/z/WarriorsMOD.jar|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/resourcepacks/dbcwarriors.zip@path=resourcepacks|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/mainmenu.json@path=config/CustomMainMenu|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/options.txt@path=mods/..|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/optionsof.txt@path=mods/..|
-https://raw.githubusercontent.com/RedeWarrior/redewarrior.github.io/main/pojav/others/servers.dat@path=mods/..
+Baixe o novo APK no discord|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/+unimixins-all-1.7.10-0.1.17.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/1.7.10bspkrsCore-universal-6.12.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Armourers-Workshop-1.7.10-0.48.5.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/CustomMainMenu-MC1.7.10-1.9.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/CustomNPC-Plus-1.9.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Dbc Warriors V1.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Dragon-Block.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/entityculling-1.6.4-mc1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JBRA.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JFamilyC-v1.2.18.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/jinryuujrmcwarriors.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JYearsC-v1.2.3.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/NBArmors-2.7.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/ninjinentities-Unofficial-1.3.5.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/OptiFine-1.7.10_HD_U_E7.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Waila-1.5.10_1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mods/PluginMod-5.0.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/z/resourcepacks/dbcwarriors.zip@path=resourcepacks|
+https://warriorsdbc.enixstudios.com/download/pojav/z/mainmenu.json@path=config/CustomMainMenu|
+https://warriorsdbc.enixstudios.com/download/pojav/z/options.txt@path=mods/..|
+https://warriorsdbc.enixstudios.com/download/pojav/z/optionsof.txt@path=mods/..|
+https://warriorsdbc.enixstudios.com/download/pojav/z/servers.dat@path=mods/..
