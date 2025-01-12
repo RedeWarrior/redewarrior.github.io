08e5e097d749bee3e3129ad134c988e8c1377b99
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2025-01-11 22:58:04 -0300

    Update mods-pojav.js

--- mods-pojav.js
+++ mods-pojav.js
@@ -1,4 +1,4 @@
-2.8 Z|
+511.01 Z|
 https://warriorsdbc.enixstudios.com/download/pojav/z/mods/+unimixins-all-1.7.10-0.1.17.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/z/mods/1.7.10bspkrsCore-universal-6.12.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Armourers-Workshop-1.7.10-0.48.5.jar|
