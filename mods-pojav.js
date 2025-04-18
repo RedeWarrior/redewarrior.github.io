3b8686b24b0a68e8f3f9d5d366f71d3045315df5
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2025-04-17 22:16:42 -0300

    Update mods-pojav.js

--- mods-pojav.js
+++ mods-pojav.js
@@ -1,24 +1,24 @@
-501.03 Z|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/+unimixins-all-1.7.10-0.1.17.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/1.7.10bspkrsCore-universal-6.12.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Armourers-Workshop-1.7.10-0.48.5.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/CustomMainMenu-MC1.7.10-1.9.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/CustomNPC-Plus-1.9.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Dbc Warriors V1.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Dragon-Block.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/entityculling-1.6.4-mc1.7.10.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JBRA.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JFamilyC-v1.2.18.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/jinryuujrmcwarriors.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/JYearsC-v1.2.3.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/NBArmors-2.7.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/ninjinentities-Unofficial-1.3.5.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/OptiFine1.7.10.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/Waila-1.5.10_1.7.10.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mods/PluginMod-5.0.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/z/resourcepacks/dbcwarriors.zip@path=resourcepacks|
-https://warriorsdbc.enixstudios.com/download/pojav/z/mainmenu.json@path=config/CustomMainMenu|
-https://warriorsdbc.enixstudios.com/download/pojav/z/options.txt@path=mods/..|
-https://warriorsdbc.enixstudios.com/download/pojav/z/optionsof.txt@path=mods/..|
-https://warriorsdbc.enixstudios.com/download/pojav/z/servers.dat@path=mods/..
+After 2025|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/+unimixins-all-1.7.10-0.1.17.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/1.7.10bspkrsCore-universal-6.12.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/Armourers-Workshop-1.7.10-0.48.5.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/CustomMainMenu-MC1.7.10-1.9.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/CustomNPC-Plus-1.9.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/Dbc Warriors V1.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/Dragon-Block.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/entityculling-1.6.4-mc1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/JBRA.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/JFamilyC-v1.2.18.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/jinryuujrmcwarriors.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/JYearsC-v1.2.3.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/NBArmors-2.7.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/ninjinentities-Unofficial-1.3.5.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/OptiFine1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/Waila-1.5.10_1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mods/PluginMod-5.0.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/resourcepacks/dbcwarriors.zip@path=resourcepacks|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/mainmenu.json@path=config/CustomMainMenu|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/options.txt@path=mods/..|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/optionsof.txt@path=mods/..|
+https://warriorsdbc.enixstudios.com/download/pojav/after2025/servers.dat@path=mods/..
