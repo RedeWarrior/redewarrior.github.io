eb5f84ee845d3db92d567d20a2687867ab7fc291
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2025-04-18 11:08:58 -0300

    Update mods-pojav.js

--- mods-pojav.js
+++ mods-pojav.js
@@ -14,12 +14,11 @@ https://warriorsdbc.enixstudios.com/download/pojav/after/mods/JinRyuus-HDSkins-M
 https://warriorsdbc.enixstudios.com/download/pojav/after/mods/JYearsC-v1.2.3.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/after/mods/NBArmors-2.7.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/after/mods/ninjinentities-Unofficial-1.3.5.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/OptiFine1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/after/mods/OptiFine_1.7.10_HD_U_E7.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/after/mods/Waila-1.5.10_1.7.10.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/after/mods/PluginMod-5.0.jar|
 https://warriorsdbc.enixstudios.com/download/pojav/after/resourcepacks/dbcwarriors.zip@path=resourcepacks|
 https://warriorsdbc.enixstudios.com/download/pojav/after/mainmenu.json@path=config/CustomMainMenu|
 https://warriorsdbc.enixstudios.com/download/pojav/after/options.txt@path=mods/..|
 https://warriorsdbc.enixstudios.com/download/pojav/after/optionsof.txt@path=mods/..|
-https://warriorsdbc.enixstudios.com/download/pojav/after/default.json@path=../controlmap/default.json|
 https://warriorsdbc.enixstudios.com/download/pojav/after/servers.dat@path=mods/..
