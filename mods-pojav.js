09ab29e923d23fb894d88ec56ba580c1911a4765
Minuscula <68911691+Nonopichy@users.noreply.github.com>
2025-10-14 19:45:19 -0300

    Update mods-pojav.js

--- mods-pojav.js
+++ mods-pojav.js
@@ -1,25 +1,25 @@
-Git After 10.07|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/+unimixins-all-1.7.10-0.1.17.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/1.7.10bspkrsCore-universal-6.12.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/Armourers-Workshop-1.7.10-0.48.5.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/CustomMainMenu-MC1.7.10-1.9.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/CustomNPC-Plus-1.9.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/Dbc Warriors V1.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/Dragon-Block.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/entityculling-1.6.4-mc1.7.10.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/JBRA.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/geckolib3-v3.0.0-warriors.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/JFamilyC-v1.2.18.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/jinryuujrmcwarriors.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/JYearsC-v1.2.3.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/NBArmors-2.7.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/ninjinentities-Unofficial-1.3.5.2.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/OptiFine_1.7.10_HD_U_E7.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/Waila-1.5.10_1.7.10.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mods/PluginMod-5.0.jar|
-https://warriorsdbc.enixstudios.com/download/pojav/after/resourcepacks/dbcwarriors.zip@path=resourcepacks|
-https://warriorsdbc.enixstudios.com/download/pojav/after/mainmenu.json@path=config/CustomMainMenu|
-https://warriorsdbc.enixstudios.com/download/pojav/after/options.txt@path=mods/..|
-https://warriorsdbc.enixstudios.com/download/pojav/after/optionsof.txt@path=mods/..|
-https://warriorsdbc.enixstudios.com/download/pojav/after/servers.dat@path=mods/..
+Git Unlimited 14.10|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/+unimixins-all-1.7.10-0.1.17.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/1.7.10bspkrsCore-universal-6.12.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/Armourers-Workshop-1.7.10-0.48.5.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/CustomMainMenu-MC1.7.10-1.9.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/CustomNPC-Plus-1.9.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/Dbc Warriors V1.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/jinryuudragonblockc-1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/entityculling-1.6.4-mc1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/JBRA.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/geckolib3-v3.0.0-warriors.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/JFamilyC-v1.2.18.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/jinryuujrmcore-1.7.10-LITE.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/JinRyuus-HDSkins-Mod-1.7.10-v1.3.1.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/JYearsC-v1.2.3.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/NBArmors-2.7.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/ninjinentities-Unofficial-1.3.5.2.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/OptiFine_1.7.10_HD_U_E7.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/Waila-1.5.10_1.7.10.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mods/PluginMod-5.0.jar|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/resourcepacks/dbcwarriors.zip@path=resourcepacks|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/mainmenu.json@path=config/CustomMainMenu|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/options.txt@path=mods/..|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/optionsof.txt@path=mods/..|
+https://warriorsdbc.enixstudios.com/download/pojav/unlimited/servers.dat@path=mods/..
